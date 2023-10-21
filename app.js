9676b3c3d45e35e9523316436b2fe7d77d5ddb32
bo <bodrye.kam@gmail.com>
2023-10-20 23:08:53 -0400

    new exercise on object enhancements

--- app.js
+++ app.js
@@ -265,3 +265,38 @@ const doubleAndReturnArgs = (array, ...numbers) => [
   ...array,
   ...numbers.map((value) => value * 2),
 ];
+
+///
+//  Object Enhancements Exercises
+// ---------------------------------------------------
+function createInstructor(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+let favoriteNumber = 42;
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!",
+};
+
+const instructor2 = {
+  firstName: "Colt",
+  sayHi() {
+    "Hi!";
+  },
+  sayBye() {
+    return this.firstName + " says bye!";
+  },
+};
+
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise.toUpperCase();
+    },
+  };
+}
